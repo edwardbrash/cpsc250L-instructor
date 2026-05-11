0527ea1c4fe9202ebe72fa904acae77ac49fb7b1
Edward Brash <brash@jlab.org>
2026-05-10 21:18:20 -0400

    Lab 9 Completion

--- labs/lab09_inheritance_polymorphism/starter_code/plant.py
+++ labs/lab09_inheritance_polymorphism/starter_code/plant.py
@@ -1,31 +1,36 @@
 class Plant:
     def __init__(self, name, height_cm):
-        pass
+        self.name = name
+        self.height_cm = height_cm
 
     def care_instructions(self):
-        pass
+        return "Plants do not need to be trimmed or watered."
 
     def __str__(self):
-        pass
+        return f"{self.name} is {self.height_cm} cm tall."
 
 
 class Flower(Plant):
     def __init__(self, name, height_cm, color):
-        pass
+        # inherit from Plant class
+        Plant.__init__(self, name, height_cm)
+        self.color = color
 
     def care_instructions(self):
-        pass
+        return "Flowers do not need to be trimmed, and should be watered weekly."
 
     def __str__(self):
-        pass
+        return f"{self.name} is {self.height_cm} cm tall and {self.color} is."
 
 
 class Vegetable(Plant):
     def __init__(self, name, height_cm, harvest_days):
-        pass
+        # inherit from Plant class
+        Plant.__init__(self, name, height_cm)
+        self.harvest_days = harvest_days
 
     def care_instructions(self):
-        pass
+        return "Vegetables should be trimmed and watered every 3 days."
 
     def __str__(self):
-        pass
+        return f"{self.name} is {self.height_cm} cm tall and takes {self.harvest_days} days to harvest."
